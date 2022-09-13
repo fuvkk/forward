5074d6ec18268b58a9f4389376c57facef61ea30
fuvkk <93823376+fuvkk@users.noreply.github.com>
2022-09-13 08:31:01 +0530

    Update and rename sample_config.py to Config.py

--- forwarder/Config.py
+++ forwarder/Config.py
@@ -12,19 +12,19 @@ if not __name__.endswith("sample_config"):
 # Create a new config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = True
-    REMOVE_TAG = False
+    REMOVE_TAG = True
 
     # REQUIRED
-    API_KEY = "617722711:AAHysCHsVcQja6LOlHa88101lKjrxez80-M"  # API key obtained from BotFather
-    OWNER_ID = "582884567"  # If you dont know, run the bot and do /id in your private chat with the bot
+    API_KEY = "5457862323:AAEWcjABMlcvzUG-JgU9wiwEajVWXLUEpbU"  # API key obtained from BotFather
+    OWNER_ID = "2056407064"  # If you dont know, run the bot and do /id in your private chat with the bot
 
     # FOR AUTOMATICALLY FORWARDING MESSAGES
-    FROM_CHATS = [-1001234704297]  # List of chat id's to forward messages from
-    TO_CHATS = [-1001128355490]  # List of chat id's to forward messages to
+    FROM_CHATS = [-1001667023505]  # List of chat id's to forward messages from
+    TO_CHATS = [-1001772207087]  # List of chat id's to forward messages to
 
     # FOR WEBHOOKS
     WEBHOOK = False
-    IP_ADDRESS = "127.0.0.1"  # Use "0.0.0.0" if using Heroku
+    IP_ADDRESS = "0.0.0.0"  # Use "0.0.0.0" if using Heroku
     URL = None  # The URL that the bot should listen to for updates
     PORT = 5000  # Port to listen on for webhooks
     CERT_PATH = None  # Path to certificate file
